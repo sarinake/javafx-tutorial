bb09c4eb8452447152fe9e7026384832877349e4
Sarina Ke <sarina.y.ke@gmail.com>
2026-09-17 16:48:14 +0800

    Add JavaFX controls and stylisation

---
+++ src/main/java/DialogBox.java
@@ -0,0 +1,24 @@
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class DialogBox extends HBox {
+
+    private final Label text;
+    private final ImageView displayPicture;
+
+    public DialogBox(String message, Image image) {
+        text = new Label(message);
+        displayPicture = new ImageView(image);
+
+        // Styling the dialog box
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        setAlignment(Pos.TOP_RIGHT);
+
+        getChildren().addAll(text, displayPicture);
+    }
+}
